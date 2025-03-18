77f36d1d7a2821852e0d373a01d4df628e5b60c2
Владислав Кузнецов <armoryx2005@mail.ru>
2025-03-18 11:29:41 +0500

    Добавлено открытие ссылок при нажатии на иконки мессенджеров

--- ANALYS_SYSTEM_APP/GUI/Main.xaml.cs
+++ ANALYS_SYSTEM_APP/GUI/Main.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -24,40 +25,70 @@ namespace ANALYS_SYSTEM_APP
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Ссылки для перехода на сайт
+        private readonly string Vk_Url = "https://vk.com/armoryx";
+        private readonly string TG_Url = "https://t.me/Armoryx";
+        private readonly string Git_Url = "https://github.com/ArxCoder/ANALYS_SYSTEM_APP";
         public MainWindow()
         {
             InitializeComponent();
+            //Путь к GIF-изображению при старте формы
             string path = $"{System.IO.Directory.GetCurrentDirectory()}/Assets/Main_gif.gif";
-            var gif = new BitmapImage(new Uri(path));
+
+            //Загрузка и установка GIF-изображения
+            BitmapImage gif = new BitmapImage(new Uri(path));
             ImageBehavior.SetAnimatedSource(GifImage, gif);
         }
 
         private void Enter_BTN_MouseEnter(object sender, MouseEventArgs e)
         {
+            //Добавление подчеркивания при наведении на надпись
             TextBlock tb = sender as TextBlock;
-            tb.TextDecorations = TextDecorations.Underline; // Добавляем подчеркивание
+            tb.TextDecorations = TextDecorations.Underline;
         }
 
         private void Enter_BTN_MouseLeave(object sender, MouseEventArgs e)
         {
+            //Удаление подчеркивания с надписи
             TextBlock tb = sender as TextBlock;
-            tb.TextDecorations = null; // Убираем подчеркивание
+            tb.TextDecorations = null; 
         }
 
         private void Enter_BTN_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            //Появление модального окна при нажатии на надпись войти
             Auth_Modal_Form.Visibility = Visibility.Visible;
         }
 
         private void Reg_BTN_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            //Нажатие на кнопку зарегистрироваться
             MessageBox.Show("Для регистрации обратитесь лично к администратору или заполните форму ниже.", "Регистрация",
                 MessageBoxButton.OKCancel, MessageBoxImage.Information);
         }
 
         private void Close_Modal_Auth_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            //Закрытие модального окна авторизации при нажатии на крестик
             Auth_Modal_Form.Visibility = Visibility.Hidden;
+        }
+
+        private void Git_IMG_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //Переход по ссылке на GIT
+            Process.Start(new ProcessStartInfo(Git_Url) { UseShellExecute = true });
+        }
+
+        private void TG_IMG_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //Переход по ссылке на Telegram
+            Process.Start(new ProcessStartInfo(TG_Url) { UseShellExecute = true });
+        }
+
+        private void Vk_IMG_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //Переход по ссылке на VK
+            Process.Start(new ProcessStartInfo(Vk_Url) { UseShellExecute = true });
         }
     }
 }
