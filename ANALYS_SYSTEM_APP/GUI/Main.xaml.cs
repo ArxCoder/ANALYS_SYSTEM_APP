f1dc204b8bde6cd9edfaad287bcf8459e441c1fd
Владислав Кузнецов <armoryx2005@mail.ru>
2025-03-18 13:04:19 +0500

    Добавление БД, создание функций авторизации и регистрации, визуальные доработки

--- ANALYS_SYSTEM_APP/GUI/Main.xaml.cs
+++ ANALYS_SYSTEM_APP/GUI/Main.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,6 +26,8 @@ namespace ANALYS_SYSTEM_APP
     /// </summary>
     public partial class MainWindow : Window
     {
+        Database database = new Database();
+        SHA256 sha256 = SHA256.Create();
         //Ссылки для перехода на сайт
         private readonly string Vk_Url = "https://vk.com/armoryx";
         private readonly string TG_Url = "https://t.me/Armoryx";
@@ -124,6 +127,139 @@ namespace ANALYS_SYSTEM_APP
             Reg_Lastname_Input.Text = string.Empty;
             Reg_Login_Input.Text = string.Empty;
             Reg_Password_Input.Text = string.Empty;
+        }
+
+        //Обработка нажатия кнопки Регистрация
+        private void Reg_Click(object sender, RoutedEventArgs e)
+        {
+            //Валидация на пустые поля
+            if (
+                String.Equals(Reg_Name_Input.Text, String.Empty) ||
+                String.Equals(Reg_Surname_Input.Text, String.Empty) ||
+                String.Equals(Reg_Lastname_Input.Text, String.Empty) ||
+                String.Equals(Reg_Login_Input.Text, String.Empty) ||
+                String.Equals(Reg_Password_Input.Text, String.Empty)
+                )
+            {
+                MessageBox.Show("Для создания заявки на регистрацию заполните все поля.", "Ошибка регистрации", MessageBoxButton.OK,
+                    MessageBoxImage.Exclamation);
+                return;
+            }
+
+            string Name = Reg_Name_Input.Text;
+            string Surname = Reg_Surname_Input.Text;
+            string Lastname = Reg_Lastname_Input.Text;
+            string Login = Reg_Login_Input.Text;
+            string Password = Reg_Password_Input.Text;
+            try
+            {
+                //Проверка на существование пользователя с таким же логином
+                User selected_User = database.User.Where(u => u.Login == Login).FirstOrDefault();
+                Registration_Request selected_request = database.Registration_Request.Where(sr => sr.Login == Login 
+                && sr.Request_Status_ID != 4).FirstOrDefault();
+                if (selected_User != null || selected_request != null)
+                {
+                    MessageBox.Show("Пользователь с таким логином уже существует.", "Ошибка регистрации", MessageBoxButton.OK,
+                        MessageBoxImage.Exclamation);
+                    return;
+                }
+
+                //Хеширование пароля
+                byte[] PasswordBytes = Encoding.UTF8.GetBytes(Password);
+                byte[] PasswordHash = sha256.ComputeHash(PasswordBytes);
+
+                string HeshedPassword = Convert.ToBase64String(PasswordHash);
+
+                //Создание заявки на регистрацию
+                Registration_Request registration_Request = new Registration_Request()
+                {
+                    Name = Name,
+                    Surname = Surname,
+                    Lastname = Lastname,
+                    Login = Login,
+                    Password = HeshedPassword,
+                    Creation_Date = DateTime.Now,
+                    Request_Status_ID = 1,
+                    Role_ID = 1
+                };
+
+                database.Registration_Request.Add(registration_Request);
+                database.SaveChanges();
+                MessageBox.Show("Заявка на регистрацию успешно создана, ожидайте вынсения решения по ней", "Успешная регистрация", 
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        //Обработка нажатия кнопки Войти
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            //Валидация на пустые поля
+            if(
+                String.Equals(Auth_Login_Input.Text, String.Empty) ||
+                String.Equals(Auth_Password_Input.Text, String.Empty)
+                )
+            {
+                MessageBox.Show(
+                    "Для авторизации заполните все поля", 
+                    "Ошибка авторизации", 
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Exclamation);
+
+                return;
+            }
+
+            string Login = Auth_Login_Input.Text;
+            string Password = Auth_Password_Input.Text;
+
+            try
+            {
+                //Валидация на существование пользователя
+                User seleced_User = database.User.Where(u => u.Login == Login).FirstOrDefault();
+                if (seleced_User == null)
+                {
+                    MessageBox.Show(
+                        "Пользователя с таким логином не существует.",
+                        "Ошибка авторизации",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Exclamation);
+
+                    return;
+                }
+
+                byte[] CurrentPassBytes = Encoding.UTF8.GetBytes(Password);
+                byte[] CurrentPassHash = sha256.ComputeHash(CurrentPassBytes);
+
+                string Current_Pass = Convert.ToBase64String(CurrentPassHash);
+
+                if (!String.Equals(Current_Pass, seleced_User.Password))
+                {
+                    MessageBox.Show(
+                        "Неверный логин или пароль.",
+                        "Ошибка авторизации",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Exclamation);
+
+                    return;
+                }
+
+                switch (seleced_User.Role_ID)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
         }
     }
 }
