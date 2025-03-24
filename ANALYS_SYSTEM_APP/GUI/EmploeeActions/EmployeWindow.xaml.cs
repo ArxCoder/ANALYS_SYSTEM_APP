b8b5372408d38174c7f3a0238771edea8a817345
Владислав Кузнецов <armoryx2005@mail.ru>
2025-03-24 12:21:44 +0500

    Бэкап БД + визуальная доработка основной формы сотрудника

--- ANALYS_SYSTEM_APP/GUI/EmploeeActions/EmployeWindow.xaml.cs
+++ ANALYS_SYSTEM_APP/GUI/EmploeeActions/EmployeWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ANALYS_SYSTEM_APP.GUI.EmploeeActions
 {
@@ -19,12 +20,65 @@ namespace ANALYS_SYSTEM_APP.GUI.EmploeeActions
     /// </summary>
     public partial class EmployeWindow : Window
     {
+        Database database = new Database();
         User current_User;
+        //Таймер для отображения текущего времени
+        DispatcherTimer CurrentTimer = new DispatcherTimer();
+        //Таймер для обновления новостей
+        DispatcherTimer RefreshNews = new DispatcherTimer();
 
         public EmployeWindow(User current_user)
         {
             InitializeComponent();
             this.current_User = current_user;
+
+            //Установка информации о пользователе
+            Username.Text = $"{current_User.User_Role.Name}: {current_User.Surname} {current_User.Name} {current_User.Lastname}";
+
+            //Установка текущего времени
+            CurrentDate.Text = DateTime.Now.ToShortDateString();
+            CurrentTimer.Tick += CurrentTimer_Tick;
+            CurrentTimer.Interval = TimeSpan.FromSeconds(1);
+            CurrentTimer.Start();
+
+            //Загрузка новостей
+            setNews();
+            RefreshNews.Tick += RefreshNews_Tick;
+            RefreshNews.Interval = TimeSpan.FromMinutes(5);
+            RefreshNews.Start();
+
+            Users_Birth_List.ItemsSource = database.User.ToList();
+        }
+
+        private void RefreshNews_Tick(object sender, EventArgs e)
+        {
+            setNews();
+        }
+
+        private void setNews()
+        {
+            News_List.ItemsSource = null;
+            News_List.Items.Clear();
+            News_List.ItemsSource = database.Organisation_News.ToList().OrderByDescending(on => on.Creation_Date);
+        }
+
+        //Получение текущего времени
+        private void CurrentTimer_Tick(object sender, EventArgs e)
+        {
+            CurrentTime.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void Logout_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBoxResult MB_dialog_Result = MessageBox.Show("Вы уверены, что хотите выйти?", "Уведомление о выходе",
+                MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (MB_dialog_Result == MessageBoxResult.Yes)
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
         }
     }
 }
